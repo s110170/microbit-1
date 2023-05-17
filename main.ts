0888f51d06b05f4cb5dfcf2bc15c609cefec60fa
s110170 <131328706+s110170@users.noreply.github.com>
2023-05-17 21:02:53 +0800

    新增更多狀態

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onGesture(Gesture.FreeFall, function () {
+    led.plot(3, 3)
+    led.plot(2, 1)
+    led.plot(1, 5)
+    led.plot(4, 2)
+    led.plot(5, 4)
+})
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.No)
     soundExpression.mysterious.playUntilDone()
